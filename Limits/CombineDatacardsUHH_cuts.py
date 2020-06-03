ae3c43ff2f58b1a2a70597930e744147e9123ef0
Steffen <steffen.albrecht@desy.de>
2020-06-03 10:28:40 +0200

    made CombineDatacardsUHH2_cuts.py python3 compatible and it now creates combined workspace which will be used by CombineHarvester

--- Limits/CombineDatacardsUHH_cuts.py
+++ Limits/CombineDatacardsUHH_cuts.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 from ROOT import *
 import ROOT
 import array, math
@@ -14,5 +15,9 @@ directory="datacards/"
 for signal in signals:
   card=directory+"CMS_jj_"+signal+cut+"_"+mass+"_13TeV__invMass"
   command="combineCards.py " +card+".txt " +card+"_afterVBFsel.txt > " +card+"_combined.txt"
-  print command
+  print(command)
   os.system( command  )
+  #create combined workspace solely for CombineHarvester
+  text2workspace_command = "text2workspace.py "+card+"_combined.txt; mv "+card+'_combined.root workspaces/'
+  print(command)
+  os.system(command)
